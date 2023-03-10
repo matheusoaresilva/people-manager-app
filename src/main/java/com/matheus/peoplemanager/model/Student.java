23af678a76762a0f131c946fea346f86cb587302
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-09 23:41:46 -0300

    Added getters and setters

--- src/main/java/com/matheus/peoplemanager/model/Student.java
+++ src/main/java/com/matheus/peoplemanager/model/Student.java
@@ -15,13 +15,68 @@ public class Student {
 
     }
 
-    public Student(Long id, String name, String email, String jobTitle, String phone, String imageUrl, String studentCode) {
-        this.id = id;
+    public Student(String name, String email, String jobTitle, String phone, String imageUrl, String studentCode) {
         this.name = name;
         this.email = email;
         this.jobTitle = jobTitle;
         this.phone = phone;
         this.imageUrl = imageUrl;
+        this.studentCode = studentCode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getStudentCode() {
+        return studentCode;
+    }
+
+    public void setStudentCode(String studentCode) {
         this.studentCode = studentCode;
     }
 }
