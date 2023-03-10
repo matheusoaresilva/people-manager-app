31ff18592ea2df36e826a1a71c225fb564fccdcf
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-09 23:39:47 -0300

    Created attributes

--- src/main/java/com/matheus/peoplemanager/model/Student.java
+++ src/main/java/com/matheus/peoplemanager/model/Student.java
@@ -1,4 +1,13 @@
 package com.matheus.peoplemanager.model;
 
+
 public class Student {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    private String studentCode;
 }
