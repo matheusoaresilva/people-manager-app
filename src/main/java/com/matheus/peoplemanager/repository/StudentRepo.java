040db6f9f95d361b406d709db58e2479af318070
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-10 00:09:06 -0300

    Added functionality to delete a student by ID in the StudentRepo class through the deleteStudentById method

--- src/main/java/com/matheus/peoplemanager/repository/StudentRepo.java
+++ src/main/java/com/matheus/peoplemanager/repository/StudentRepo.java
@@ -4,4 +4,5 @@ import com.matheus.peoplemanager.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepo extends JpaRepository<Student, Long> {
+    void deleteStudentById(Long id);
 }
