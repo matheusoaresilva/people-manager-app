f0900f35a6971b06bb658362f8adc8b9667578af
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-10 00:07:16 -0300

    Added CRUD functionality to manage students, implemented methods to find all students, update a specific student, and delete a student from system

--- src/main/java/com/matheus/peoplemanager/service/StudentService.java
+++ src/main/java/com/matheus/peoplemanager/service/StudentService.java
@@ -5,6 +5,7 @@ import com.matheus.peoplemanager.repository.StudentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -19,5 +20,17 @@ public class StudentService {
     public Student addStudent(Student student){
         student.setStudentCode(UUID.randomUUID().toString());
         return studentRepo.save(student);
+    }
+
+    public List<Student> findAllStudents(){
+        return studentRepo.findAll();
+    }
+
+    public Student updateStudent(Student student){
+        return studentRepo.save(student);
+    }
+
+    public void deleteStudent(Long id){
+        studentRepo.deleteById(id);
     }
 }
