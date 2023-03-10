0f7bc426dfdfd9b0bab37fa28c1d885a652bba48
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-10 00:14:35 -0300

    Added functionality to find a student by ID in the StudentRepo class

--- src/main/java/com/matheus/peoplemanager/repository/StudentRepo.java
+++ src/main/java/com/matheus/peoplemanager/repository/StudentRepo.java
@@ -3,6 +3,10 @@ package com.matheus.peoplemanager.repository;
 import com.matheus.peoplemanager.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StudentRepo extends JpaRepository<Student, Long> {
     void deleteStudentById(Long id);
+
+    Optional<Student> findStudentById(Long id);
 }
