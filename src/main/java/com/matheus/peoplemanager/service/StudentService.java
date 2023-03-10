adadc241cd8d39513986ae65295b3e757ba38b8f
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-09 23:57:20 -0300

    Added functionality to add students via the addStudent method in the StudentService class. The student code is automatically generated through the UUID class and the student data is saved in the database through the save method of the studentRepo object

--- src/main/java/com/matheus/peoplemanager/service/StudentService.java
+++ src/main/java/com/matheus/peoplemanager/service/StudentService.java
@@ -1,4 +1,23 @@
 package com.matheus.peoplemanager.service;
 
+import com.matheus.peoplemanager.model.Student;
+import com.matheus.peoplemanager.repository.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class StudentService {
+    private final StudentRepo studentRepo;
+
+    @Autowired
+    public StudentService(StudentRepo studentRepo){
+        this.studentRepo = studentRepo;
+    }
+
+    public Student addStudent(Student student){
+        student.setStudentCode(UUID.randomUUID().toString());
+        return studentRepo.save(student);
+    }
 }
