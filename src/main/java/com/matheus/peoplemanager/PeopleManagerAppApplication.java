fb221bcc5dad6fef6e03cdd78239f039778f2a95
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-09 23:31:53 -0300

    first commit

---
+++ src/main/java/com/matheus/peoplemanager/PeopleManagerAppApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.peoplemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleManagerAppApplication.class, args);
+	}
+
+}
