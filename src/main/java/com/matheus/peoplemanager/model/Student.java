361c22248dc7cb0cb4fcc04fba13669fd0978087
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-09 23:43:37 -0300

    Mapping

--- src/main/java/com/matheus/peoplemanager/model/Student.java
+++ src/main/java/com/matheus/peoplemanager/model/Student.java
@@ -1,14 +1,23 @@
 package com.matheus.peoplemanager.model;
 
 
-public class Student {
+import jakarta.persistence.*;
 
+import java.io.Serializable;
+
+@Entity
+public class Student implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
     private Long id;
     private String name;
     private String email;
     private String jobTitle;
     private String phone;
     private String imageUrl;
+    @Column(nullable = false, updatable = false)
     private String studentCode;
 
     public Student(){
